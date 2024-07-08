8997d4316b72ec6717cd6c634d80b75e509dd557
Bojana Popov <96697478+popovbojana@users.noreply.github.com>
2024-07-08 23:14:27 +0200

    feat: add login activity (#2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,14 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+    implementation(libs.jwtdecode)
+
 }
